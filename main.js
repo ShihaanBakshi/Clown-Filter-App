7a6f2720e8ebe9f1483e199dbc3d3505c5330521
ShihaanBakshi <87414764+ShihaanBakshi@users.noreply.github.com>
2021-12-11 15:52:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+noseX=0;
+noseY=0;
+
+function preload(){
+    clown_nose=loadImage('https://i.postimg.cc/wTpSCssj/clownn-ose.png');
+}
+
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',getPoses);
+}
+
+function modelLoaded(){
+console.log('poseNet is initialized');
+}
+
+function draw(){
+image(video,0,0,300,300);
+fill(255,0,0);
+stroke(255,0,0);
+image(clown_nose,noseX,noseY,30,30)
+}
+
+function take_snapshot(){
+save('img.png');
+} 
+
+function getPoses(results){
+if(results.length>0){
+    console.log(results);
+    noseX=results[0].pose.nose.x-15;
+    noseY=results[0].pose.nose.y-15;
+    console.log("nose X= "+results[0].pose.nose.x);
+    console.log("nose Y= "+results[0].pose.nose.y);
+}
+}
